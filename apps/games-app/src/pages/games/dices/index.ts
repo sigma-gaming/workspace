e5066f7780a5b2204b628392d28478e9afda40ca
Evgenii Zakharov <github@risen.dev>
2024-01-20 07:55:53 +0400

    move dices page under authentication

--- apps/games-app/src/pages/games/dices/index.ts
+++ apps/games-app/src/pages/games/dices/index.ts
@@ -1,8 +1,9 @@
 import { routes } from '../../../routing'
 import { RouteRecord } from '../../../routing/types.ts'
 import { DicesGamePageView } from './view.tsx'
+import { createAuthenticatedRoute } from '../../../routing/authenticated-route.tsx'
 
-export const DicesGameRoute: RouteRecord = {
+export const DicesGameRoute = createAuthenticatedRoute({
   route: routes.dicesGame,
   view: DicesGamePageView,
-}
+})
