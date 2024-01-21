ba716b3cec63285a53d3525bd916ee52dd702d69
Evgenii Zakharov <github@risen.dev>
2024-01-21 12:50:36 +0400

    better logs

--- apps/games-api/src/shared/logger/index.ts
+++ apps/games-api/src/shared/logger/index.ts
@@ -1,11 +1,39 @@
 import { createLogger } from '@neodx/log'
 import { createHttpLogger } from '@neodx/log/http'
 import { json, pretty } from '@neodx/log/node'
+import { RawReplyDefaultExpression, RawRequestDefaultExpression } from 'fastify'
 import { v4 as uuid } from 'uuid'
+import { Connect } from 'vite'
 import { env } from '../env'
+import IncomingMessage = Connect.IncomingMessage
 
-export const httpLogger = createHttpLogger({
-  simple: !env.isProd,
+function serializeReq(req: IncomingMessage) {
+  const sessionCookieIndex = req.headers.cookie?.indexOf('session') ?? -1
+
+  const cookie =
+    sessionCookieIndex !== -1
+      ? req.headers.cookie?.slice(sessionCookieIndex, sessionCookieIndex + 500)
+      : req.headers.cookie
+
+  return {
+    id: req.id,
+    url: req.url,
+    method: req.method,
+    headers: {
+      'host': req.headers.host,
+      'user-agent': req.headers['user-agent'],
+      'origin': req.headers.origin,
+      'x-trace-id': req.headers['x-trace-id'],
+      cookie,
+    },
+  }
+}
+
+export const httpLogger = createHttpLogger<
+  RawRequestDefaultExpression,
+  RawReplyDefaultExpression
+>({
+  simple: false,
   shouldLogRequest: true,
   getRequestId: (req) => {
     const existingID = req.headers['x-trace-id']
@@ -14,6 +42,14 @@ export const httpLogger = createHttpLogger({
     req.headers['x-trace-id'] = id
     return id
   },
+  getRequestMeta: (ctx) => ({
+    req: serializeReq(ctx.req),
+  }),
+  getResponseMeta: (ctx) => ({
+    statusCode: ctx.res.statusCode,
+    responseTime: ctx.responseTime,
+    req: serializeReq(ctx.req),
+  }),
 })
 
 export const logger = createLogger({
