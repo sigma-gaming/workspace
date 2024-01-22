5dbfe68ba97fb4e63693f9be1521ba85184b4ace
Evgenii Zakharov <github@risen.dev>
2024-01-23 00:50:09 +0400

    fix

--- apps/games-api/src/app.ts
+++ apps/games-api/src/app.ts
@@ -68,7 +68,7 @@ app.get('/container', async () => {
 app.get('/health', async (_, reply) => {
   const response = await fetch(`${process.env.INTERNAL_MAINTENANCE_URL}/get`)
   const { value } = await response.json()
-  if (!value) return reply.status(503).send()
+  if (value) return reply.status(503).send()
   return 'Healthy'
 })
 
--- apps/games-app/server.js
+++ apps/games-app/server.js
@@ -55,8 +55,7 @@ app.get('/container', async () => {
 app.get('/health', async (_, reply) => {
   const response = await fetch(`${process.env.INTERNAL_MAINTENANCE_URL}/get`)
   const { value } = await response.json()
-  console.log({ value })
-  if (!value) return reply.status(503).send()
+  if (value) return reply.status(503).send()
   return 'Healthy'
 })
 
