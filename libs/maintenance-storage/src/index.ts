23af0854349c607fac164117fd990e0beec2ffd7
Evgenii Zakharov <github@risen.dev>
2024-01-25 04:55:01 +0400

    retries when getting maintenance mode

--- libs/maintenance-storage/src/index.ts
+++ libs/maintenance-storage/src/index.ts
@@ -6,9 +6,23 @@ export function createMaintenanceStorage(redisUrl = process.env.REDIS_URL) {
 
   return {
     async isMaintenanceMode() {
-      if (client.status !== 'ready') return true
-      const value = await client.get('global:maintenance')
-      return value === 'true'
+      if (client.status !== 'ready') {
+        return true
+      }
+
+      for (let i = 0; i < 3; i++) {
+        try {
+          const value = await client.get('global:maintenance')
+          return value === 'true'
+        } catch (error) {
+          console.log('[Maintenance Storage] Failed to get maintenance mode:')
+          console.error(error)
+          await new Promise((resolve) => setTimeout(resolve, 1000))
+          console.log('[Maintenance Storage] Retrying in 1 second...')
+        }
+      }
+
+      return true
     },
     async setMaintenanceMode(value: boolean) {
       if (client.status !== 'ready') return null
