c3aa7917d9d448da4ce107e9328cf637bae78332
Evgenii Zakharov <github@risen.dev>
2024-01-25 04:47:45 +0400

    auto reload after 5 sec

--- apps/games-app/src/features/maintenance/model.ts
+++ apps/games-app/src/features/maintenance/model.ts
@@ -1,5 +1,6 @@
 import { createEffect, createEvent, createStore, sample } from 'effector'
 import Cookies from 'js-cookie'
+import { delay } from 'patronum'
 import { env } from '../../shared/env'
 
 /**
@@ -7,19 +8,41 @@ import { env } from '../../shared/env'
  * (in case when API healthcheck was executed before APP healthcheck)
  */
 const saveToCookieFx = createEffect(() => {
-  const expires = new Date()
-  expires.setSeconds(expires.getSeconds() + 5)
-  Cookies.set('maintenance', String(true), { domain: env.domain, expires })
+  const reloadAt = new Date()
+  reloadAt.setSeconds(reloadAt.getSeconds() + 5)
+
+  Cookies.set('maintenanceReloadAt', reloadAt.toISOString(), {
+    domain: env.domain,
+    expires: reloadAt,
+  })
+})
+
+const reloadPageFx = createEffect(() => {
+  window.location.reload()
 })
 
 const activate = createEvent()
 
-const initialActive = Cookies.get('maintenance') === 'true'
-const $active = createStore(initialActive).on(activate, () => true)
+const reloadAt = Cookies.get('maintenanceReloadAt')
+const $active = createStore(Boolean(reloadAt)).on(activate, () => true)
 
 sample({
   clock: activate,
   target: saveToCookieFx,
+})
+
+const msBeforeReload = reloadAt
+  ? new Date(reloadAt).getTime() - new Date().getTime()
+  : 5000
+
+const requestedReload = sample({
+  source: delay($active, msBeforeReload),
+  filter: Boolean,
+})
+
+sample({
+  clock: requestedReload,
+  target: reloadPageFx,
 })
 
 export const $$maintenance = {
