ceefea9c67b5d02a01e4e06ca819487a277a7887
Evgenii Zakharov <github@risen.dev>
2024-02-04 16:33:53 +0400

    fix: callback page width

--- apps/games-app/src/pages/callbacks/vk/view.tsx
+++ apps/games-app/src/pages/callbacks/vk/view.tsx
@@ -2,7 +2,7 @@ import { Loader, Text } from '@mantine/core'
 
 export const VkCallbackPageView = () => {
   return (
-    <div className="flex flex-col items-center gap-8">
+    <div className="flex flex-col items-center gap-8 max-w-60 md:max-w-80">
       <Loader size="xl" />
       <Text className="text-center" size="lg" lh="xs">
         Выполняется вход через&nbsp;VK
