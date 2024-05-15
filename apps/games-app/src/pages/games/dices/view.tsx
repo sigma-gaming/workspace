f96df2fa8e8cf0b85edcbd574435d30bfaa9b7a0
Evgenii Zakharov <github@risen.dev>
2024-05-15 13:04:18 +0500

    wip: dice selector

--- apps/games-app/src/pages/games/dices/view.tsx
+++ apps/games-app/src/pages/games/dices/view.tsx
@@ -1,5 +1,13 @@
-import { Button, Card, MultiSelect, NumberInput, Title } from '@mantine/core'
+import {
+  Button,
+  Card,
+  InputError,
+  InputLabel,
+  NumberInput,
+  Title,
+} from '@mantine/core'
 import { useRive } from '@rive-app/react-canvas'
+import clsx from 'clsx'
 import { useUnit } from 'effector-react'
 import { memo, useEffect } from 'react'
 import { $$dicesPage } from './model.ts'
@@ -23,7 +31,6 @@ const AnimatedDice = memo(() => {
 
 export const DicesGamePageView = () => {
   const bet = useUnit($$dicesPage.fields.bet.$value)
-  const sides = useUnit($$dicesPage.fields.sides.$value)
   const errors = useUnit($$dicesPage.form.$errors)
   const playing = useUnit($$dicesPage.$playing)
   const autoplaying = useUnit($$dicesPage.$autoplaying)
@@ -52,21 +59,9 @@ export const DicesGamePageView = () => {
         decimalScale={2}
       />
 
-      <MultiSelect
-        label="Грани"
-        data={[
-          { label: '1', value: '1' },
-          { label: '2', value: '2' },
-          { label: '3', value: '3' },
-          { label: '4', value: '4' },
-          { label: '5', value: '5' },
-          { label: '6', value: '6' },
-        ]}
-        value={sides}
-        onChange={(sides) => $$dicesPage.fields.sides.update(sides)}
-        error={errors.sides[0]}
-        disabled={autoplaying}
-      />
+      <InputLabel>Грани</InputLabel>
+      <SidesSelect />
+      {errors.sides[0] && <InputError>{errors.sides[0]}</InputError>}
 
       <Button type="submit" disabled={playing || autoplaying} fullWidth={true}>
         Играть
@@ -75,5 +70,46 @@ export const DicesGamePageView = () => {
         {autoplaying ? 'Остановить' : 'Автоигра'}
       </Button>
     </Card>
+  )
+}
+
+const SidesSelect = () => {
+  const sides = useUnit($$dicesPage.fields.sides.$value)
+  const update = useUnit($$dicesPage.fields.sides.update)
+  const autoplaying = useUnit($$dicesPage.$autoplaying)
+
+  const options = [
+    { label: '1', value: '1' },
+    { label: '2', value: '2' },
+    { label: '3', value: '3' },
+    { label: '4', value: '4' },
+    { label: '5', value: '5' },
+    { label: '6', value: '6' },
+  ]
+
+  return (
+    <ul className="grid grid-rows-2 grid-flow-col gap-4">
+      {options.map(({ value }) => (
+        <li key={value} className="block">
+          <button
+            type="button"
+            className={clsx(
+              'flex justify-center items-center w-full aspect-square bg-white rounded-xl',
+              sides.includes(value) ? 'opacity-100' : 'opacity-25',
+            )}
+            onClick={() =>
+              update(
+                sides.includes(value)
+                  ? sides.filter((side) => side !== value)
+                  : sides.concat(value),
+              )
+            }
+            disabled={autoplaying}
+          >
+            <span className="block w-3 h-3 rounded-full bg-[#1B1C2F]" />
+          </button>
+        </li>
+      ))}
+    </ul>
   )
 }
