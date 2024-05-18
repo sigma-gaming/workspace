4aadd02696bc81a4c0891cd0123fede41123e9e9
Evgenii Zakharov <github@risen.dev>
2024-05-19 00:21:03 +0500

    feat: dice dots and some improvements

--- apps/games-app/src/pages/games/dices/view.tsx
+++ apps/games-app/src/pages/games/dices/view.tsx
@@ -88,15 +88,12 @@ const SidesSelect = () => {
   ]
 
   return (
-    <ul className="grid grid-rows-2 grid-flow-col gap-4">
+    <ul className="grid grid-cols-3 grid-flow-row gap-4">
       {options.map(({ value }) => (
         <li key={value} className="block">
           <button
             type="button"
-            className={clsx(
-              'flex justify-center items-center w-full aspect-square bg-white rounded-xl',
-              sides.includes(value) ? 'opacity-100' : 'opacity-25',
-            )}
+            className="w-full outline-none"
             onClick={() =>
               update(
                 sides.includes(value)
@@ -106,10 +103,93 @@ const SidesSelect = () => {
             }
             disabled={autoplaying}
           >
-            <span className="block w-3 h-3 rounded-full bg-[#1B1C2F]" />
+            <Dice
+              className={sides.includes(value) ? 'opacity-100' : 'opacity-25'}
+              side={Number(value)}
+            />
           </button>
         </li>
       ))}
     </ul>
+  )
+}
+
+interface DiceProps {
+  className?: string
+  side: number
+}
+
+const Dice = ({ className, side }: DiceProps) => {
+  return (
+    <div
+      className={clsx(
+        className,
+        'relative flex justify-center items-center w-full aspect-square bg-white rounded-[16%]',
+      )}
+    >
+      {side === 1 && (
+        <DiceDot className="top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2" />
+      )}
+
+      {side === 2 && (
+        <>
+          <DiceDot className="top-[30%] left-[30%]" />
+          <DiceDot className="bottom-[30%] right-[30%]" />
+        </>
+      )}
+
+      {side === 3 && (
+        <>
+          <DiceDot className="top-[22%] left-[22%]" />
+          <DiceDot className="right-[22%] bottom-[22%]" />
+          <DiceDot className="top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2" />
+        </>
+      )}
+
+      {side === 4 && (
+        <>
+          <DiceDot className="top-[24%] left-[24%]" />
+          <DiceDot className="top-[24%] right-[24%]" />
+          <DiceDot className="bottom-[24%] left-[24%]" />
+          <DiceDot className="bottom-[24%] right-[24%]" />
+        </>
+      )}
+
+      {side === 5 && (
+        <>
+          <DiceDot className="top-[22%] left-[22%]" />
+          <DiceDot className="top-[22%] right-[22%]" />
+          <DiceDot className="bottom-[22%] left-[22%]" />
+          <DiceDot className="bottom-[22%] right-[22%]" />
+          <DiceDot className="top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2" />
+        </>
+      )}
+
+      {side === 6 && (
+        <>
+          <DiceDot className="top-[18%] left-[24%]" />
+          <DiceDot className="top-[18%] right-[24%]" />
+          <DiceDot className="bottom-[18%] left-[24%]" />
+          <DiceDot className="bottom-[18%] right-[24%]" />
+          <DiceDot className="top-[50%] left-[24%] -translate-y-1/2" />
+          <DiceDot className="top-[50%] right-[24%] -translate-y-1/2" />
+        </>
+      )}
+    </div>
+  )
+}
+
+interface DiceDotProps {
+  className?: string
+}
+
+const DiceDot = ({ className }: DiceDotProps) => {
+  return (
+    <span
+      className={clsx(
+        className,
+        'absolute block min-w-3 min-h-3 w-[16%] h-[16%] rounded-full bg-[#1B1C2F]',
+      )}
+    />
   )
 }
