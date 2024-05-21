19ddc68126498049d452607beacf6e12fda82e00
Evgenii Zakharov <github@risen.dev>
2024-05-22 01:47:20 +0500

    fix: migrations

--- dbs/games-db/src/migrate.ts
+++ dbs/games-db/src/migrate.ts
@@ -3,12 +3,20 @@ import { drizzle } from 'drizzle-orm/postgres-js'
 import { migrate } from 'drizzle-orm/postgres-js/migrator'
 import postgres from 'postgres'
 
-const migrationsFolder = join(process.cwd(), '../../dbs/games-db/drizzle')
+const migrationsFolder =
+  process.env.NODE_ENV === 'development'
+    ? join(process.cwd(), '../../dbs/games-db/drizzle')
+    : join(process.cwd(), 'dbs/games-db/drizzle')
 
 export async function migrateGamesDB(databaseUrl: string) {
-  const psql = postgres(databaseUrl, { max: 1 })
-  const db = drizzle(psql)
-  console.log({ migrationsFolder })
-  await migrate(db, { migrationsFolder })
-  await psql.end()
+  try {
+    const psql = postgres(databaseUrl, { max: 1 })
+    const db = drizzle(psql)
+    await migrate(db, { migrationsFolder })
+    await psql.end()
+  } catch (error) {
+    console.info('[GamesDB] Failed to migrate database')
+    console.error(error)
+    process.exit(1)
+  }
 }
