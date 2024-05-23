c0876990ee58ffd1add6f8f6b0c08e277814f454
Evgenii Zakharov <github@risen.dev>
2024-05-23 16:42:06 +0500

    test: add some log

--- apps/games-api/src/app.ts
+++ apps/games-api/src/app.ts
@@ -56,6 +56,17 @@ app.get('/ready', async (_, reply) => {
   return 'Ready'
 })
 
+setInterval(() => {
+  console.log(
+    JSON.stringify({
+      level: 5,
+      short_message: 'Test',
+      host: 'test',
+      version: '10.4.1',
+    }),
+  )
+}, 1000)
+
 cronJobRegistry.register('SyncBudget', syncBudgetJob).start()
 
 migrateGamesDB(env.database.url)
