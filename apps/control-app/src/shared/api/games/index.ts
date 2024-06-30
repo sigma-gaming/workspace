e7772aae045fe5357e8125f87dae172f629a6cf0
Evgenii Zakharov <github@risen.dev>
2024-06-30 21:58:44 +0500

    fix: include credentials in fetch

--- apps/control-app/src/shared/api/games/index.ts
+++ apps/control-app/src/shared/api/games/index.ts
@@ -2,4 +2,8 @@ import { ApiType } from '@apis/games-api'
 import { hc } from 'hono/client'
 import { env } from '../../env'
 
-export const gamesApi = hc<ApiType>(env.gamesApi.url)
+export const gamesApi = hc<ApiType>(env.gamesApi.url, {
+  fetch(input: RequestInfo | URL, requestInit?: RequestInit) {
+    return fetch(input, { ...requestInit, credentials: 'include' })
+  },
+})
