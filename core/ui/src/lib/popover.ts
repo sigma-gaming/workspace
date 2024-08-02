7a6880e872b72e085309f268881f7dc9d0712701
Evgenii Zakharov <github@risen.dev>
2024-08-02 15:48:13 +0500

    fix: types

--- core/ui/src/lib/popover.ts
+++ core/ui/src/lib/popover.ts
@@ -1,8 +1,6 @@
-import { PopoverMiddlewares } from '@mantine/core/lib/components/Popover/Popover.types'
+import { FlipOptions } from '@floating-ui/react'
 
-export function createFlipOptions(
-  options: PopoverMiddlewares['flip'] & object = {},
-): PopoverMiddlewares['flip'] {
+export function createFlipOptions(options: FlipOptions = {}): FlipOptions {
   return {
     padding: { top: 72, bottom: 64, left: 16, right: 16 },
     fallbackPlacements: ['top', 'right', 'bottom'],
