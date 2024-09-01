86f17fb1dbdd8c5f708e305668b44bf4086b8dc5
Evgenii Zakharov <github@risen.dev>
2024-09-01 15:57:57 +0500

    fix: getTasks without session

--- apps/games-api/src/routes/tasks/global/get-tasks.ts
+++ apps/games-api/src/routes/tasks/global/get-tasks.ts
@@ -1,10 +1,7 @@
-import { globalTaskService, sessionService } from '@games/services'
+import { globalTaskService } from '@games/services'
 import { createRouter } from '../../../hono'
 
 export const getTasksRoute = createRouter().get('/', async (ctx) => {
-  const session = ctx.get('session')
-  sessionService.getUser(session)
-
   const tasks = await globalTaskService.getTasks()
   return ctx.json(tasks)
 })
