84610e65539f2d0ea2eb237e2fd8c81f8b1b139b
Evgenii Zakharov <github@risen.dev>
2024-10-27 02:05:23 +0500

    feat: add early hints

--- apps/letsauth/astro.config.mjs
+++ apps/letsauth/astro.config.mjs
@@ -1,11 +1,12 @@
 import { esbuildDecorators } from '@anatine/esbuild-decorators'
 import node from '@astrojs/node'
 import tailwind from '@astrojs/tailwind'
+import earlyHints from '@itsmatteomanf/astro-early-hints'
 import { defineConfig } from 'astro/config'
 
 // eslint-disable-next-line import-x/no-default-export
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), earlyHints()],
   output: 'hybrid',
   adapter: node({ mode: 'standalone' }),
   server: {
