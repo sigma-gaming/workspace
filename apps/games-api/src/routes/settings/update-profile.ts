4a99f3a8d6f0bb65021d87b49b67059324476a0d
Evgenii Zakharov <github@risen.dev>
2024-10-28 19:29:59 +0500

    feat: reserved usernames

--- apps/games-api/src/routes/settings/update-profile.ts
+++ apps/games-api/src/routes/settings/update-profile.ts
@@ -2,13 +2,104 @@ import { BadRequestException } from '@core/exceptions'
 import { zValidator } from '@core/server'
 import { gamesDb } from '@dbs/games-db'
 import { AccountTable, ProfileTable, ProfileUpdate } from '@dbs/games-schema'
-import { AccountProvider } from '@dbs/games-types'
+import { AccountProvider, UserRole } from '@dbs/games-types'
 import { getUserFullName, ProfileValidation } from '@games/model'
 import { gamesCaches } from '@games/redis'
-import { profileService, sessionService } from '@games/services'
+import {
+  profileService,
+  roleService,
+  sessionService,
+  userService,
+} from '@games/services'
 import { eq } from 'drizzle-orm'
 import { z } from 'zod'
 import { createRouter } from '../../hono'
+
+const reservedUsernames = [
+  'sigma',
+  'sigmadmin',
+  'sigma_admin',
+  'sigma_support',
+  'sigma_moderator',
+  'sigma_developer',
+  'sigma_dev',
+  'sigma_owner',
+  'sigma_bot',
+  'sigma_service',
+  'sigma_team',
+  'sigma_staff',
+  'sigmaadmin',
+  'sigma_helpdesk',
+  'sigma_system',
+  'admin',
+  'administrator',
+  'moderator',
+  'support',
+  'helpdesk',
+  'system',
+  'root',
+  'superuser',
+  'manager',
+  'operator',
+  'team',
+  'staff',
+  'service',
+  'developer',
+  'dev',
+  'owner',
+  'bot',
+  'api',
+  'test',
+  'master',
+  'security',
+  'info',
+  'contact',
+  'no-reply',
+  'notifications',
+  'noreply',
+  'official',
+  'webmaster',
+  'help',
+  'agent',
+  'customer_service',
+  'assistant',
+  'user_support',
+  'community',
+  'guide',
+  'consultant',
+  'agent',
+  'moderation',
+  'moderators',
+  'project',
+  'control',
+  'account',
+  'client_service',
+  'sysadmin',
+  'team_leader',
+  'group_admin',
+  'contact_support',
+  'customer_care',
+  'management',
+  'host',
+  'monitor',
+  'api_service',
+  'backend',
+  'frontend',
+  'engineer',
+  'developer_support',
+  'server',
+  'dashboard',
+  'console',
+  'admin_panel',
+  'config',
+  'analytics',
+  'update',
+  'access',
+  'report',
+  'auth',
+  'authorization',
+  'authentication',
+]
 
 export const updateProfileRoute = createRouter().post(
   '/',
@@ -23,8 +114,16 @@ export const updateProfileRoute = createRouter().post(
   async (ctx) => {
     const payload = ctx.req.valid('json')
     const { userId } = sessionService.getHonoSession(ctx)
+    const user = await userService.getUser(userId)
+    const isAdmin = roleService.hasRole(user, UserRole.Admin)
 
     if (payload.username) {
+      if (!isAdmin && reservedUsernames.includes(payload.username))
+        throw new BadRequestException({
+          path: ['username'],
+          message: 'Недопустимый никнейм',
+        })
+
       const currentProfile = await gamesDb.query.ProfileTable.findFirst({
         where: eq(ProfileTable.username, payload.username),
       })
