b157f9d60099666ddbf6dd97f7b5f8292444b6f6
Evgenii Zakharov <github@risen.dev>
2024-11-01 10:56:28 +0500

    feat: index on Promocode.expiresAt

--- dbs/games-schema/src/promocode.ts
+++ dbs/games-schema/src/promocode.ts
@@ -1,6 +1,7 @@
 import { PromocodeBonus } from '@dbs/games-types'
 import {
   boolean,
+  index,
   integer,
   json,
   pgTable,
@@ -11,32 +12,38 @@ import {
 import { promocodeBonusTypeEnum } from './enums'
 import { UserTable } from './user'
 
-export const PromocodeTable = pgTable('Promocode', {
-  id: uuid('id').defaultRandom().primaryKey(),
-  createdAt: timestamp('createdAt', { withTimezone: true, mode: 'string' })
-    .notNull()
-    .defaultNow(),
-  expiresAt: timestamp('expiresAt', {
-    withTimezone: true,
-    mode: 'string',
-  }),
+export const PromocodeTable = pgTable(
+  'Promocode',
+  {
+    id: uuid('id').defaultRandom().primaryKey(),
+    createdAt: timestamp('createdAt', { withTimezone: true, mode: 'string' })
+      .notNull()
+      .defaultNow(),
+    expiresAt: timestamp('expiresAt', {
+      withTimezone: true,
+      mode: 'string',
+    }),
 
-  campaign: text('campaign'),
-  code: text('code').unique().notNull(),
-  wageringMultiplier: integer('wageringMultiplier').notNull(),
-  usages: integer('usages').notNull(),
-  maxUsages: integer('maxUsages').notNull(),
-  isActive: boolean('isActive').notNull(),
-  bonusType: promocodeBonusTypeEnum('bonusType').notNull(),
-  bonus: json('bonus').$type<PromocodeBonus>().notNull(),
+    campaign: text('campaign'),
+    code: text('code').unique().notNull(),
+    wageringMultiplier: integer('wageringMultiplier').notNull(),
+    usages: integer('usages').notNull(),
+    maxUsages: integer('maxUsages').notNull(),
+    isActive: boolean('isActive').notNull(),
+    bonusType: promocodeBonusTypeEnum('bonusType').notNull(),
+    bonus: json('bonus').$type<PromocodeBonus>().notNull(),
 
-  userId: uuid('userId').references(() => UserTable.id, {
-    onDelete: 'cascade',
-  }),
+    userId: uuid('userId').references(() => UserTable.id, {
+      onDelete: 'cascade',
+    }),
 
-  createdBy: uuid('createdBy').references(() => UserTable.id, {
-    onDelete: 'set null',
+    createdBy: uuid('createdBy').references(() => UserTable.id, {
+      onDelete: 'set null',
+    }),
+  },
+  (table) => ({
+    expiresAtIdx: index().on(table.expiresAt),
   }),
-})
+)
 
 export type PromocodeSelect = typeof PromocodeTable.$inferSelect
