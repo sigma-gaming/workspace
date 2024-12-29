0582b6b29b9840b3d6d250c1aa42cb7e268e8280
Evgenii Zakharov <github@risen.dev>
2024-12-30 01:17:39 +0500

    fix: games-api metrics

--- apps/games-api/src/metrics/metrics.ts
+++ apps/games-api/src/metrics/metrics.ts
@@ -2,37 +2,23 @@ import { Counter, Histogram } from 'prom-client'
 import { registry } from './registry'
 
 export const betsAmountCounter = new Counter({
-  name: 'games_api_bets_amount',
+  name: 'games_bets_amount',
   help: 'Amount of bets',
   labelNames: ['game'],
 })
 
-export const winsAmountCounter = new Counter({
-  name: 'games_api_wins_amount',
-  help: 'Amount of wins',
-  labelNames: ['game'],
-})
-
-export const lossesAmountCounter = new Counter({
-  name: 'games_api_losses_amount',
-  help: 'Amount of losses',
-  labelNames: ['game'],
-})
-
 export const diceSidesHistogram = new Histogram({
-  name: 'games_api_dice_sides',
+  name: 'games_dice_sides',
   help: 'Dice sides',
   buckets: [1, 2, 3, 4, 5],
 })
 
 export const pincodeGamesCounter = new Counter({
-  name: 'games_api_pincode_games',
-  help: 'Amount of pincode games',
+  name: 'games_pincode_games',
+  help: 'Number of pincode games by mode',
   labelNames: ['mode'],
 })
 
 registry.registerMetric(betsAmountCounter)
-registry.registerMetric(winsAmountCounter)
-registry.registerMetric(lossesAmountCounter)
 registry.registerMetric(diceSidesHistogram)
 registry.registerMetric(pincodeGamesCounter)
--- apps/games-api/src/metrics/updaters.ts
+++ apps/games-api/src/metrics/updaters.ts
@@ -1,18 +1,7 @@
 import { GameRecordSelect } from '@dbs/games-schema'
-import { GameOutcome } from '@dbs/games-types'
-import {
-  betsAmountCounter,
-  lossesAmountCounter,
-  winsAmountCounter,
-} from './metrics'
+import { betsAmountCounter } from './metrics'
 
 export function updateBetMetrics(record: GameRecordSelect) {
-  const { game, bet, outcome, payout } = record
+  const { game, bet } = record
   betsAmountCounter.inc({ game }, bet)
-
-  if (outcome === GameOutcome.Win) {
-    winsAmountCounter.inc({ game }, payout)
-  } else {
-    lossesAmountCounter.inc({ game }, bet)
-  }
 }
