ef614b3f3002ca6231a153f830c8fe46cc766218
Evgenii Zakharov <github@risen.dev>
2024-12-31 04:51:55 +0500

    fix: sentry bug issue

--- apps/games-tasks/src/shared/sentry/init.ts
+++ apps/games-tasks/src/shared/sentry/init.ts
@@ -7,4 +7,11 @@ export const sentry = Sentry.init({
   environment: env.stage,
   release: env.gamesTasks.version,
   tracesSampleRate: 1,
+
+  // TODO: Issue with standard instrumentation:
+  // https://github.com/getsentry/sentry-javascript/issues/12891
+  // https://github.com/oven-sh/bun/issues/13165
+  defaultIntegrations: Sentry.getDefaultIntegrations({}).filter(
+    (i) => i.name !== 'Http',
+  ),
 })
