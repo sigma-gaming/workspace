0ab6c44c5b9dd9900f8bd4f6768852e054737f77
Evgenii Zakharov <github@risen.dev>
2025-01-03 02:39:41 +0500

    Update cron for InitializeChatMessages job

--- apps/games-tasks/src/jobs/initialize-chat-messages.ts
+++ apps/games-tasks/src/jobs/initialize-chat-messages.ts
@@ -3,6 +3,6 @@ import { createJob } from '../shared/jobs'
 
 export const initializeChatMessagesJob = createJob({
   name: 'InitializeChatMessages',
-  cronTime: '0 */1 * * *', // every 1 hour
+  cronTime: '*/10 * * * *', // every 10 minutes
   handler: () => chatService.initializeMessages(),
 })
