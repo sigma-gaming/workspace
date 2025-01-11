9390e77f82701f0291eb16fbfd624c63b3f1c92d
Evgenii Zakharov <github@risen.dev>
2025-01-12 00:41:56 +0500

    Update SW text

--- apps/games-app/src/sw.ts
+++ apps/games-app/src/sw.ts
@@ -53,8 +53,8 @@ const fallbackHtml = `
   <section>
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="orange" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="width: 96px; height: 96px;"><path d="M4 7m0 1a1 1 0 0 1 1 -1h14a1 1 0 0 1 1 1v7a1 1 0 0 1 -1 1h-14a1 1 0 0 1 -1 -1z"></path><path d="M7 16v4"></path><path d="M7.5 16l9 -9"></path><path d="M13.5 16l6.5 -6.5"></path><path d="M4 13.5l6.5 -6.5"></path><path d="M17 16v4"></path><path d="M5 20h4"></path><path d="M15 20h4"></path><path d="M17 7v-2"></path><path d="M7 7v-2"></path></svg>
     <h1>Сайт недоступен</h1>
-    <p>У вас отсутствует подключение к&nbsp;интернету, или сайт заблокирован в&nbsp;вашем&nbsp;регионе</p>
-    <p>Получите актуальный домен в&nbsp;нашем Telegram&nbsp;боте: <a href="https://t.me/SigmaGamesBot">@SigmaGamesBot</a></p>
+    <p>Возможные причины: отсутствие подключения к&nbsp;интернету, технические неполадки, блокировка сайта в&nbsp;вашем&nbsp;регионе</p>
+    <p>Актуальный домен можно получить в&nbsp;нашем Telegram&nbsp;боте: <a href="https://t.me/SigmaGamesBot">@SigmaGamesBot</a></p>
   </section>
 </body>`
 
