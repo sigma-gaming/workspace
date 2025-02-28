4b9e59924bd3f110d7800cbeac227b174c8143d7
Evgenii Zakharov <github@risen.dev>
2025-02-28 20:20:51 +0500

    fix

--- scripts/inject-env.mjs
+++ scripts/inject-env.mjs
@@ -10,7 +10,7 @@ const script = `window.PUBLIC_ENV = ${JSON.stringify(ENV)}`
 
 const indexHtmlPath = '/app/index.html'
 const content = fs.readFileSync(indexHtmlPath, { encoding: 'utf8' })
-const toReplace = '<script type="module" src="/env.js"></script>'
+const toReplace = '<!-- public-env -->'
 const replaceWith = `<script>${script}</script>`
 const withEnv = content.replace(toReplace, replaceWith)
 fs.writeFileSync(indexHtmlPath, withEnv)
