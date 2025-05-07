4f25e7e21850d57f16b220ab9ddef5989ed95a4c
Evgenii Zakharov <github@risen.dev>
2025-05-07 07:55:49 +0500

    feat: better ws reconnections, updated openapi

--- apps/games-app/src/shared/api/core-ws/model.ts
+++ apps/games-app/src/shared/api/core-ws/model.ts
@@ -2,6 +2,7 @@ import { createSubscriptionFactory } from '@core/client'
 import { SocketRejectionReason } from '@core/exceptions'
 import { HubConnectionBuilder, JsonHubProtocol } from '@microsoft/signalr'
 import { createEffect, createEvent, createStore, sample } from 'effector'
+import { condition, interval, not } from 'patronum'
 import { env } from '../../env'
 import { Event, EventName } from './generated'
 
@@ -27,16 +28,46 @@ const connected = createEvent()
 const disconnected = createEvent()
 const rejected = createEvent<SocketRejectionReason>()
 
-sample({ source: connect, target: connectFx })
-sample({ source: disconnect, target: disconnectFx })
-sample({ source: reconnect, target: reconnectFx })
+const $shouldBeConnected = createStore(false)
+  .on(connect, () => true)
+  .on(disconnect, () => false)
 
 const $connected = createStore(false)
   .on(connected, () => true)
   .on(disconnected, () => false)
 
+const { tick: tryReconnect } = interval({
+  start: connect,
+  stop: disconnect,
+  timeout: 5000,
+})
+
 connection.onreconnected(() => connected())
 connection.onclose(() => disconnected())
+
+sample({
+  source: reconnect,
+  filter: $shouldBeConnected,
+  target: reconnectFx,
+})
+
+condition({
+  source: $shouldBeConnected,
+  if: Boolean,
+  then: connectFx,
+  else: disconnectFx,
+})
+
+sample({
+  clock: connectFx.done,
+  target: connected,
+})
+
+sample({
+  clock: tryReconnect,
+  filter: not($connected),
+  target: reconnect,
+})
 
 const subscriptionFactory = createSubscriptionFactory<EventName, Event>(
   connection,
